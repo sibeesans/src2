dbfa1dda79b6b9a71bd34187658b59e5db357bc6
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-08 06:28:02 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -20,7 +20,7 @@ commonname=none
 email=admin@bahenol
 
 # simple password minimal
-curl -sS https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
+curl -sS https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -128,7 +128,7 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/nginx.conf"
 mkdir -p /home/vps/public_html
 /etc/init.d/nginx restart
 
@@ -244,18 +244,18 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # banner /etc/issue.net
 sleep 1
 echo -e "[ ${green}INFO$NC ] Settings banner"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/myridwan/src/ipuk/issue.net"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/sibeesans/src/ipuk/issue.net"
 chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 cat> /etc/issue.net << END
 <font color="red"><b>============================</b></font><br> 
-<font color="white"><b>        RIDWAN-STORE         </b></font><br> 
+<font color="white"><b>        Bg Vpn Service         </b></font><br> 
 <font color="red"><b>============================</b></font>
 END
 
 #install bbr dan optimasi kernel
-wget https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
 # blockir torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
@@ -276,27 +276,27 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O usernew "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/usernew.sh"
-wget -O hapus "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/hapus.sh"
-wget -O member "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/member.sh"
-wget -O renew "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/renew.sh"
-wget -O cek "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/cek.sh"
-wget -O add-host "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/add-host.sh"
-wget -O speedtest "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/speedtest_cli.py"
-wget -O xp "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/xp.sh"
-wget -O asu "https://raw.githubusercontent.com/myridwan/src/ipuk/asu.sh"
-wget -O menu "https://raw.githubusercontent.com/myridwan/src/ipuk/menu.sh"
-wget -O sshws "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/sshws.sh"
-wget -O trial "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/trial.sh"
-wget -O ssh-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all/ssh-menu.sh"
-wget -O v2ray-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all/v2ray-menu.sh"
-wget -O trojan-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all/trojan-menu.sh"
-wget -O ssgrpc-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all/ssgrpc-menu.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/myridwan/src/ipuk/xray/cek-ws.sh"
-wget -O about "https://raw.githubusercontent.com/myridwan/sc/ipuk/ssh/about.sh" 
-wget -O running "https://raw.githubusercontent.com/myridwan/sc/ipuk/ssh/running.sh"
-wget -O banner "https://raw.githubusercontent.com/myridwan/src/ipuk/banner.sh"
-wget -O del-tr "https://raw.githubusercontent.com/myridwan/src/ipuk/xray/del-tr.sh"
+wget -O usernew "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/usernew.sh"
+wget -O hapus "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/member.sh"
+wget -O renew "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/renew.sh"
+wget -O cek "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/cek.sh"
+wget -O add-host "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/add-host.sh"
+wget -O speedtest "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/speedtest_cli.py"
+wget -O xp "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/xp.sh"
+wget -O asu "https://raw.githubusercontent.com/sibeesans/src/ipuk/asu.sh"
+wget -O menu "https://raw.githubusercontent.com/sibeesans/src/ipuk/menu.sh"
+wget -O sshws "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/sshws.sh"
+wget -O trial "https://raw.githubusercontent.com/sibeesans/src/ipuk/ssh/trial.sh"
+wget -O ssh-menu "https://raw.githubusercontent.com/sibeesans/vip/ipuk/menu_all/ssh-menu.sh"
+wget -O v2ray-menu "https://raw.githubusercontent.com/sibeesans/vip/ipuk/menu_all/v2ray-menu.sh"
+wget -O trojan-menu "https://raw.githubusercontent.com/sibeesans/vip/ipuk/menu_all/trojan-menu.sh"
+wget -O ssgrpc-menu "https://raw.githubusercontent.com/sibeesans/vip/ipuk/menu_all/ssgrpc-menu.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/sibeesans/src/ipuk/xray/cek-ws.sh"
+wget -O about "https://raw.githubusercontent.com/sibeesans/sc/ipuk/ssh/about.sh" 
+wget -O running "https://raw.githubusercontent.com/sibeesans/sc/ipuk/ssh/running.sh"
+wget -O banner "https://raw.githubusercontent.com/sibeesans/src/ipuk/banner.sh"
+wget -O del-tr "https://raw.githubusercontent.com/sibeesans/src/ipuk/xray/del-tr.sh"
 chmod +x usernew
 chmod +x menu
 chmod +x hapus
